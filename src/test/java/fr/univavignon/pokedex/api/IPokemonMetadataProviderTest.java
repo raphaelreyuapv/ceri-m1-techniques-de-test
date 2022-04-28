b6dd81879170721d331ce7d4daaab6a1d1f35eb7
Raphael Rey <raphael.rey@alumni.univ-avignon.fr>
2022-04-28 14:52:18 +0200

    Ajout test unitaire pokemonmetadata

--- src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokemonMetadataProviderTest.java
@@ -1,15 +1,71 @@
 package fr.univavignon.pokedex.api;
 
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class IPokemonMetadataProviderTest {
     private PokemonMetadata pm;
+    private IPokemonMetadataProvider pmp;
+
     @Before
     public void init(){
+        pmp = mock(IPokemonMetadataProvider.class);
+        try {
+            pm = new PokemonMetadata(0, "Bulbizare", 126, 126, 90);
+            when(pmp.getPokemonMetadata(0)).thenReturn(pm);
+        }catch(Exception e){
 
+        }
     }
 
+    @Test
+    public void shouldReturnBulbizare(){
+        try {
+            PokemonMetadata test = pmp.getPokemonMetadata(0);
+            assertEquals(test.getName(), pm.getName());
+        }catch(Exception e){
 
+        }
+    }
+
+    @Test
+    public void shouldReturnIndex(){
+        try {
+            PokemonMetadata test = pmp.getPokemonMetadata(0);
+            assertEquals(test.getIndex(), pm.getIndex());
+        }catch(Exception e){
+
+        }
+    }
+    @Test
+    public void shouldReturnAttack(){
+        try {
+            PokemonMetadata test = pmp.getPokemonMetadata(0);
+            assertEquals(test.getAttack(), pm.getAttack());
+        }catch(Exception e){
+
+        }
+    }
+    @Test
+    public void shouldReturnDefense(){
+        try {
+            PokemonMetadata test = pmp.getPokemonMetadata(0);
+            assertEquals(test.getAttack(), pm.getDefense());
+        }catch(Exception e){
+
+        }
+    }
+    @Test
+    public void shouldReturnStamina(){
+        try {
+            PokemonMetadata test = pmp.getPokemonMetadata(0);
+            assertEquals(test.getStamina(), pm.getStamina());
+        }catch(Exception e){
+
+        }
+    }
 }
